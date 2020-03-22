6a79e144999a8bf391d9a7cbcf1014a4c5f99fe6
halil <halilyilmaz.py@gmail.com>
2020-03-22 17:03:42 +0300

    Add project files.

---
+++ project/Login.Designer.cs
@@ -0,0 +1,385 @@
+﻿namespace project
+{
+	partial class Login
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.button6 = new System.Windows.Forms.Button();
+			this.button7 = new System.Windows.Forms.Button();
+			this.button8 = new System.Windows.Forms.Button();
+			this.button9 = new System.Windows.Forms.Button();
+			this.button10 = new System.Windows.Forms.Button();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.denemeDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+			this.denemeDataSet = new project.denemeDataSet();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button11 = new System.Windows.Forms.Button();
+			this.button12 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.panel3 = new System.Windows.Forms.Panel();
+			this.panel4 = new System.Windows.Forms.Panel();
+			this.panel5 = new System.Windows.Forms.Panel();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.denemeDataSetBindingSource)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.denemeDataSet)).BeginInit();
+			this.groupBox1.SuspendLayout();
+			this.groupBox2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.button1.Font = new System.Drawing.Font("Century Gothic", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.button1.Location = new System.Drawing.Point(15, 36);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 75);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.secilibutonlar);
+			// 
+			// button2
+			// 
+			this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.button2.Font = new System.Drawing.Font("Century Gothic", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.button2.Location = new System.Drawing.Point(118, 36);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 75);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "2";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.secilibutonlar);
+			// 
+			// button3
+			// 
+			this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.button3.Font = new System.Drawing.Font("Century Gothic", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.button3.Location = new System.Drawing.Point(225, 36);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 75);
+			this.button3.TabIndex = 2;
+			this.button3.Text = "3";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.secilibutonlar);
+			// 
+			// button4
+			// 
+			this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.button4.Font = new System.Drawing.Font("Century Gothic", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.button4.Location = new System.Drawing.Point(15, 128);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(75, 75);
+			this.button4.TabIndex = 3;
+			this.button4.Text = "4";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.secilibutonlar);
+			// 
+			// button5
+			// 
+			this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.button5.Font = new System.Drawing.Font("Century Gothic", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.button5.Location = new System.Drawing.Point(118, 128);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(75, 75);
+			this.button5.TabIndex = 4;
+			this.button5.Text = "5";
+			this.button5.UseVisualStyleBackColor = true;
+			this.button5.Click += new System.EventHandler(this.secilibutonlar);
+			// 
+			// button6
+			// 
+			this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.button6.Font = new System.Drawing.Font("Century Gothic", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.button6.Location = new System.Drawing.Point(225, 128);
+			this.button6.Name = "button6";
+			this.button6.Size = new System.Drawing.Size(75, 75);
+			this.button6.TabIndex = 5;
+			this.button6.Text = "6";
+			this.button6.UseVisualStyleBackColor = true;
+			this.button6.Click += new System.EventHandler(this.secilibutonlar);
+			// 
+			// button7
+			// 
+			this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.button7.Font = new System.Drawing.Font("Century Gothic", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.button7.Location = new System.Drawing.Point(15, 228);
+			this.button7.Name = "button7";
+			this.button7.Size = new System.Drawing.Size(75, 75);
+			this.button7.TabIndex = 6;
+			this.button7.Text = "7";
+			this.button7.UseVisualStyleBackColor = true;
+			this.button7.Click += new System.EventHandler(this.secilibutonlar);
+			// 
+			// button8
+			// 
+			this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.button8.Font = new System.Drawing.Font("Century Gothic", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.button8.Location = new System.Drawing.Point(118, 228);
+			this.button8.Name = "button8";
+			this.button8.Size = new System.Drawing.Size(75, 75);
+			this.button8.TabIndex = 7;
+			this.button8.Text = "8";
+			this.button8.UseVisualStyleBackColor = true;
+			this.button8.Click += new System.EventHandler(this.secilibutonlar);
+			// 
+			// button9
+			// 
+			this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.button9.Font = new System.Drawing.Font("Century Gothic", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.button9.Location = new System.Drawing.Point(225, 228);
+			this.button9.Name = "button9";
+			this.button9.Size = new System.Drawing.Size(75, 75);
+			this.button9.TabIndex = 8;
+			this.button9.Text = "9";
+			this.button9.UseVisualStyleBackColor = true;
+			this.button9.Click += new System.EventHandler(this.secilibutonlar);
+			// 
+			// button10
+			// 
+			this.button10.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.button10.Font = new System.Drawing.Font("Century Gothic", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.button10.Location = new System.Drawing.Point(118, 325);
+			this.button10.Name = "button10";
+			this.button10.Size = new System.Drawing.Size(75, 75);
+			this.button10.TabIndex = 9;
+			this.button10.Text = "0";
+			this.button10.UseVisualStyleBackColor = true;
+			this.button10.Click += new System.EventHandler(this.secilibutonlar);
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(131, 199);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(153, 29);
+			this.comboBox1.TabIndex = 10;
+			// 
+			// denemeDataSetBindingSource
+			// 
+			this.denemeDataSetBindingSource.DataSource = this.denemeDataSet;
+			this.denemeDataSetBindingSource.Position = 0;
+			// 
+			// denemeDataSet
+			// 
+			this.denemeDataSet.DataSetName = "denemeDataSet";
+			this.denemeDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(131, 252);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.PasswordChar = '•';
+			this.textBox1.Size = new System.Drawing.Size(153, 27);
+			this.textBox1.TabIndex = 11;
+			// 
+			// button11
+			// 
+			this.button11.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.button11.Location = new System.Drawing.Point(60, 313);
+			this.button11.Name = "button11";
+			this.button11.Size = new System.Drawing.Size(75, 75);
+			this.button11.TabIndex = 12;
+			this.button11.Text = "Login";
+			this.button11.UseVisualStyleBackColor = true;
+			this.button11.Click += new System.EventHandler(this.button11_Click);
+			// 
+			// button12
+			// 
+			this.button12.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.button12.Location = new System.Drawing.Point(186, 313);
+			this.button12.Name = "button12";
+			this.button12.Size = new System.Drawing.Size(75, 75);
+			this.button12.TabIndex = 13;
+			this.button12.Text = "Clear";
+			this.button12.UseVisualStyleBackColor = true;
+			this.button12.Click += new System.EventHandler(this.button12_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(32, 202);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(88, 21);
+			this.label1.TabIndex = 14;
+			this.label1.Text = "Username";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(32, 258);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(82, 21);
+			this.label2.TabIndex = 15;
+			this.label2.Text = "Password";
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.button3);
+			this.groupBox1.Controls.Add(this.button1);
+			this.groupBox1.Controls.Add(this.button2);
+			this.groupBox1.Controls.Add(this.button4);
+			this.groupBox1.Controls.Add(this.button5);
+			this.groupBox1.Controls.Add(this.button6);
+			this.groupBox1.Controls.Add(this.button7);
+			this.groupBox1.Controls.Add(this.button10);
+			this.groupBox1.Controls.Add(this.button8);
+			this.groupBox1.Controls.Add(this.button9);
+			this.groupBox1.Font = new System.Drawing.Font("Century Gothic", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.groupBox1.Location = new System.Drawing.Point(34, 32);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(319, 406);
+			this.groupBox1.TabIndex = 16;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Number";
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.groupBox2.Controls.Add(this.pictureBox1);
+			this.groupBox2.Controls.Add(this.button12);
+			this.groupBox2.Controls.Add(this.label2);
+			this.groupBox2.Controls.Add(this.comboBox1);
+			this.groupBox2.Controls.Add(this.label1);
+			this.groupBox2.Controls.Add(this.textBox1);
+			this.groupBox2.Controls.Add(this.button11);
+			this.groupBox2.Font = new System.Drawing.Font("Century Gothic", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.groupBox2.Location = new System.Drawing.Point(381, 32);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(335, 406);
+			this.groupBox2.TabIndex = 17;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "Login";
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(64)))), ((int)(((byte)(52)))));
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+			this.panel1.Location = new System.Drawing.Point(0, 0);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(750, 32);
+			this.panel1.TabIndex = 10;
+			// 
+			// panel3
+			// 
+			this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(64)))), ((int)(((byte)(52)))));
+			this.panel3.Location = new System.Drawing.Point(0, 32);
+			this.panel3.Name = "panel3";
+			this.panel3.Size = new System.Drawing.Size(28, 111);
+			this.panel3.TabIndex = 19;
+			// 
+			// panel4
+			// 
+			this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(64)))), ((int)(((byte)(52)))));
+			this.panel4.Location = new System.Drawing.Point(722, 32);
+			this.panel4.Name = "panel4";
+			this.panel4.Size = new System.Drawing.Size(28, 111);
+			this.panel4.TabIndex = 20;
+			// 
+			// panel5
+			// 
+			this.panel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(64)))), ((int)(((byte)(52)))));
+			this.panel5.Location = new System.Drawing.Point(354, 32);
+			this.panel5.Name = "panel5";
+			this.panel5.Size = new System.Drawing.Size(28, 111);
+			this.panel5.TabIndex = 20;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = global::project.Properties.Resources.avatar__2_;
+			this.pictureBox1.Location = new System.Drawing.Point(108, 26);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(153, 149);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 16;
+			this.pictureBox1.TabStop = false;
+			// 
+			// Login
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(750, 470);
+			this.Controls.Add(this.panel5);
+			this.Controls.Add(this.panel4);
+			this.Controls.Add(this.panel3);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.groupBox2);
+			this.Controls.Add(this.groupBox1);
+			this.Name = "Login";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Login_Load);
+			((System.ComponentModel.ISupportInitialize)(this.denemeDataSetBindingSource)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.denemeDataSet)).EndInit();
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox2.ResumeLayout(false);
+			this.groupBox2.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.Button button6;
+		private System.Windows.Forms.Button button7;
+		private System.Windows.Forms.Button button8;
+		private System.Windows.Forms.Button button9;
+		private System.Windows.Forms.Button button10;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button11;
+		private System.Windows.Forms.Button button12;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.GroupBox groupBox2;
+		private System.Windows.Forms.BindingSource denemeDataSetBindingSource;
+		private denemeDataSet denemeDataSet;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Panel panel3;
+		private System.Windows.Forms.Panel panel4;
+		private System.Windows.Forms.Panel panel5;
+	}
+}
+
